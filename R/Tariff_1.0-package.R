e77acd8b9c0f974779bcde5bcee342c73e7e078d
Zehang Li <lizehang@uw.edu>
2016-03-03 05:50:02 +0000

    version 1.0

---
+++ R/Tariff_1.0-package.R
@@ -0,0 +1,20 @@
+
+#' 400 records of Sample Input
+#' 
+#' This is a dataset consisting of 400 arbitrary sample input deaths randomly sampled from cleaned PHMRC data.
+#' 
+#' 
+#' @name RandomVA3
+#' @docType data
+#' @format 400 arbitrary input records.
+#' @keywords datasets
+#' @examples
+#' 
+#' data(RandomVA3)
+#' head(RandomVA3$train)
+#' head(RandomVA3$test)
+
+NULL
+
+
+
